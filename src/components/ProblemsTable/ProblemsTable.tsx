7a33ea027246154f51dcf5935932f0db2a1afc8a
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-08-27 23:22:02 +0530

    fetching problems from the databse

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -1,18 +1,26 @@
-import { problems } from "@/mockProblems/problems";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { BsCheckCircle } from "react-icons/bs";
 import { AiFillYoutube } from "react-icons/ai";
 import { IoMdClose } from "react-icons/io";
 import YouTube from "react-youtube";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { firestore } from "@/firebase/firebase";
+import { DBProblem } from "@/utils/types/problem";
 
-type ProblemsTableProps = {};
+type ProblemsTableProps = {
+  setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-const ProblemsTable: React.FC<ProblemsTableProps> = () => {
+const ProblemsTable: React.FC<ProblemsTableProps> = ({
+  setLoadingProblems,
+}) => {
   const [youtubePlayer, setYoutubePlayer] = useState({
     isOpen: false,
     videoId: "",
   });
+
+  let problems = useGetProblemss(setLoadingProblems);
 
   const closeModal = () => {
     setYoutubePlayer({
@@ -31,17 +39,17 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
   return (
     <>
       <tbody className="text-white">
-        {problems.map((doc, idx) => {
+        {problems.map((problem, idx) => {
           const difficultyColor =
-            doc.difficulty === "Easy"
+            problem.difficulty === "Easy"
               ? "text-dark-green-s"
-              : doc.difficulty === "Medium"
+              : problem.difficulty === "Medium"
               ? "text-dark-yellow"
               : "text-dark-pink";
           return (
             <tr
               className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
-              key={doc.id}
+              key={problem.id}
             >
               <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
                 <BsCheckCircle fontSize={"18"} width={"18"} />
@@ -49,24 +57,24 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
               <td className="px-6 py-4">
                 <Link
                   className="hover:text-blue-600 cursor-pointer"
-                  href={`/problems/${doc.id}`}
+                  href={`/problems/${problem.id}`}
                 >
-                  {doc.title}
+                  {problem.title}
                 </Link>
               </td>
               <td className={`px-6 py-4 ${difficultyColor}`}>
-                {doc.difficulty}
+                {problem.difficulty}
               </td>
-              <td className="px-6 py-4">{doc.category}</td>
+              <td className="px-6 py-4">{problem.category}</td>
               <td className="px-6 py-4">
-                {doc.videoId ? (
+                {problem.videoId ? (
                   <AiFillYoutube
                     fontSize={"28"}
                     className="cursor-pointer hover:text-red-600"
                     onClick={() =>
                       setYoutubePlayer({
                         isOpen: true,
-                        videoId: doc.videoId as string,
+                        videoId: problem.videoId as string,
                       })
                     }
                   />
@@ -106,3 +114,28 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
   );
 };
 export default ProblemsTable;
+
+function useGetProblemss(
+  setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  const [problems, setProblems] = useState<DBProblem[]>([]);
+  useEffect(() => {
+    const getProblems = async () => {
+      setLoadingProblems(true);
+      const q = query(
+        collection(firestore, "problems"),
+        orderBy("order", "asc")
+      );
+      const querySnapshot = await getDocs(q);
+      const tmp: DBProblem[] = [];
+      querySnapshot.forEach((doc) => {
+        tmp.push({ id: doc.id, ...doc.data() } as DBProblem);
+      });
+      setProblems(tmp);
+      setLoadingProblems(false);
+    };
+    getProblems();
+  }, [setLoadingProblems]);
+
+  return problems;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [loadingProblems, setLoadingProblems] = useState(true);
+  console.log(loadingProblems);
+
   return (
     <>
       <main className="bg-dark-layer-2 min-h-screen">
@@ -11,31 +15,130 @@ export default function Home() {
         </h1>
 
         <div className="relative overflow-x-auto mx-auto px-6 pb-10">
+          {loadingProblems && (
+            <div className="animate-pulse max-w-[1200px] mx-auto sm:w-7/12 wfull">
+              {[...Array(10)].map((_, idx) => (
+                <LoadingSkeleton key={idx} />
+              ))}
+            </div>
+          )}
           <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
-            <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
-              <tr>
-                <th scope="col" className="px-1 py-3 w-0 font-medium">
-                  Status
-                </th>
-                <th scope="col" className="px-6 py-3 w-0 font-medium">
-                  Title
-                </th>
-                <th scope="col" className="px-6 py-3 w-0 font-medium">
-                  Difficulty
-                </th>
+            {!loadingProblems && (
+              <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
+                <tr>
+                  <th scope="col" className="px-1 py-3 w-0 font-medium">
+                    Status
+                  </th>
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Title
+                  </th>
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Difficulty
+                  </th>
 
-                <th scope="col" className="px-6 py-3 w-0 font-medium">
-                  Category
-                </th>
-                <th scope="col" className="px-6 py-3 w-0 font-medium">
-                  Solution
-                </th>
-              </tr>
-            </thead>
-            <ProblemsTable />
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Category
+                  </th>
+                  <th scope="col" className="px-6 py-3 w-0 font-medium">
+                    Solution
+                  </th>
+                </tr>
+              </thead>
+            )}
+            <ProblemsTable setLoadingProblems={setLoadingProblems} />
           </table>
         </div>
       </main>
     </>
   );
+}
+
+const LoadingSkeleton = () => {
+  return (
+    <div className="flex items-center space-x-12 mt-4 px-6">
+      <div className="w-6 h-6 shrink-0 rounded-full bg-dark-layer-1"></div>
+      <div className="h-4 sm:w-52 w-32 rounded-full bg-dark-layer-1"></div>
+      <div className="h-4 sm:w-52 w-32 rounded-full bg-dark-layer-1"></div>
+      <div className="h-4 sm:w-52 w-32 rounded-full bg-dark-layer-1"></div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+};
+
+// form and function to add problems to db
+// import { firestore } from "@/firebase/firebase";
+// import { doc, setDoc } from "firebase/firestore";
+{
+  /*
+  const [inputs, setInputs] = useState({
+    id: "",
+    title: "",
+  difficulty: "",
+  category: "",
+  videoId: "",
+  link: "",
+  order: "",
+  likes: 0,
+  dislikes: 0,
+});
+
+const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setInputs({ ...inputs, [e.target.name]: e.target.value });
+};
+
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  const newProblem = { ...inputs, order: Number(inputs.order) };
+  await setDoc(doc(firestore, "problems", inputs.id), newProblem);
+  alert("saved");}
+  */
+}
+{
+  /* <form className="p-6 flex flex-col max-w-sm gap-3">
+  <input
+  onChange={handleInputChange}
+  type="text"
+  placeholder="problem id"
+  name="id"
+  />
+  <input
+  onChange={handleInputChange}
+  type="text"
+  placeholder="title"
+  name="title"
+  />
+  <input
+  onChange={handleInputChange}
+  type="text"
+  placeholder="difficulty"
+  name="difficulty"
+  />
+  <input
+  onChange={handleInputChange}
+  type="text"
+  placeholder="category"
+  name="category"
+  />
+  <input
+  onChange={handleInputChange}
+  type="text"
+  placeholder="order"
+  name="order"
+  />
+  <input
+  onChange={handleInputChange}
+  type="text"
+  placeholder="videoId"
+  name="videoId"
+  />
+  <input
+  onChange={handleInputChange}
+  type="text"
+  placeholder="link"
+  name="link"
+  />
+  <button className="bg-white" onClick={handleSubmit}>
+    Save to Db
+  </button>
+</form> */
 }
