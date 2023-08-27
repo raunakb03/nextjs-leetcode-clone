9fc2a5f5e64c3812dca09211534369b3142ea5c8
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-08-28 00:00:01 +0530

    showing likes and dislikes from database and showing skeleton on loading

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -1,15 +1,22 @@
-import { Problem } from "@/utils/types/problem";
+import { useState, useEffect } from "react";
+import { DBProblem, Problem } from "@/utils/types/problem";
 import Image from "next/image";
 import React from "react";
 import { AiFillLike, AiFillDislike } from "react-icons/ai";
 import { BsCheck2Circle } from "react-icons/bs";
 import { TiStarOutline } from "react-icons/ti";
+import { doc, getDoc } from "firebase/firestore";
+import { firestore } from "@/firebase/firebase";
+import RectangleSkeleton from "@/components/skeletons/RectangleSkeleton";
+import CircleSkeleton from "@/components/skeletons/CircleSkeleton";
 
 type ProblemDescriptionProps = {
   problem: Problem;
 };
 
 const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
+  const { currentProblem, loading, problemDifficultyClass } =
+    useGetCurrentProblem(problem.id);
   return (
     <div className="bg-dark-layer-1">
       {/* TAB */}
@@ -32,27 +39,39 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
                 {problem.title}
               </div>
             </div>
-            <div className="flex items-center mt-3">
-              <div
-                className={`text-olive bg-olive inline-block rounded-[21px] bg-opacity-[.15] px-2.5 py-1 text-xs font-medium capitalize `}
-              >
-                Easy
+            {!loading && currentProblem && (
+              <div className="flex items-center mt-3">
+                <div
+                  className={`${problemDifficultyClass} inline-block rounded-[21px] bg-opacity-[.15] px-2.5 py-1 text-xs font-medium capitalize `}
+                >
+                  {currentProblem.difficulty}
+                </div>
+                <div className="rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-green-s text-dark-green-s">
+                  <BsCheck2Circle />
+                </div>
+                <div className="flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors duration-200 text-dark-gray-6">
+                  <AiFillLike />
+                  <span className="text-xs">{currentProblem.likes}</span>
+                </div>
+                <div className="flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors duration-200 text-green-s text-dark-gray-6">
+                  <AiFillDislike />
+                  <span className="text-xs">{currentProblem.dislikes}</span>
+                </div>
+                <div className="cursor-pointer hover:bg-dark-fill-3  rounded p-[3px]  ml-4 text-xl transition-colors duration-200 text-green-s text-dark-gray-6 ">
+                  <TiStarOutline />
+                </div>
               </div>
-              <div className="rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-green-s text-dark-green-s">
-                <BsCheck2Circle />
+            )}
+
+            {loading && (
+              <div className="mt-3 flex space-x-2">
+                <RectangleSkeleton />
+                <CircleSkeleton />
+                <RectangleSkeleton />
+                <RectangleSkeleton />
+                <CircleSkeleton />
               </div>
-              <div className="flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors duration-200 text-dark-gray-6">
-                <AiFillLike />
-                <span className="text-xs">120</span>
-              </div>
-              <div className="flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors duration-200 text-green-s text-dark-gray-6">
-                <AiFillDislike />
-                <span className="text-xs">2</span>
-              </div>
-              <div className="cursor-pointer hover:bg-dark-fill-3  rounded p-[3px]  ml-4 text-xl transition-colors duration-200 text-green-s text-dark-gray-6 ">
-                <TiStarOutline />
-              </div>
-            </div>
+            )}
 
             {/* Problem Statement(paragraphs) */}
             <div className="text-white text-sm">
@@ -107,3 +126,32 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
   );
 };
 export default ProblemDescription;
+
+function useGetCurrentProblem(problemId: string) {
+  const [currentProblem, setCurrentProblem] = useState<DBProblem | null>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [problemDifficultyClass, setProblemDifficultyClass] =
+    useState<string>("");
+  useEffect(() => {
+    const getCurrentProblem = async () => {
+      setLoading(true);
+      const docRef = doc(firestore, "problems", problemId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const problem = docSnap.data();
+        setCurrentProblem({ id: docSnap.id, ...docSnap.data() } as DBProblem);
+        setProblemDifficultyClass(
+          problem.difficulty === "Easy"
+            ? "bg-olive text-olive"
+            : problem.difficulty === "Medium"
+            ? "bg-dark-yellow text-dark-yellow"
+            : "bg-dark-pink text-dark-pink"
+        );
+      }
+      setLoading(false);
+    };
+    getCurrentProblem();
+  }, [problemId]);
+
+  return { currentProblem, loading, problemDifficultyClass };
+}
